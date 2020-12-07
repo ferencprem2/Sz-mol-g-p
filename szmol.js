be5e2a04a04a7a5a29a0e07276d4e55e2443b6d8
ferencprem2 <you@example.com>
2020-12-07 17:17:29 +0100

    kész

---
+++ szmol.js
@@ -0,0 +1,18 @@
+function össz() {
+    document.szamol.er.value = document.szamol.x1.value * 1 +
+        document.szamol.x2.value * 1
+}
+
+function kiv() {
+    document.szamol.er.value = document.szamol.x1.value -
+        document.szamol.x2.value
+}
+
+function szor() {
+    document.szamol.er.value = document.szamol.x1.value *
+        document.szamol.x2.value
+}
+function oszt() {
+    document.szamol.er.value = document.szamol.x1.value /
+        document.szamol.x2.value
+}
